282a261065f1960b0d3b61fc00e8a70e75f18f39
Albert Mends <mendsalbert@gmail.com>
2024-03-21 08:43:21 +0000

    eco-air-api

--- app/api/endpoints/countries.py
+++ app/api/endpoints/countries.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 import httpx
 from app.db.database import get_database
+from app.core.config import settings
 
 router = APIRouter()
 @router.get("/fetch_countries")
@@ -12,7 +13,7 @@ async def fetch_countries():
             raise HTTPException(status_code=400, detail="Error fetching data from OpenAQ")
         data = response.json()
         if "results" in data:
-            await db["countries"].insert_many(data["results"])
+            await db[settings.collection_countries].insert_many(data["results"])
             return {"message": "Countries data stored successfully in MongoDB."}
         else:
             return {"message": "No countries data found."}
